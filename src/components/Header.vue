879177019ef49fd2fd0140ee732433b404e0e65e
Franpan94 <francescopanico94@libero.it>
2022-07-26 14:37:23 +0200

    finish header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
-   <header class="ms_bg">
+   <header class="ms_bg ms_position_relative">
      <Navbar />
+     <div class="ms_clr_wht ms_position_absolute">
+         <h1>SEM Campaigns Made <br> Simple With Avada</h1>
+         <p class="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br> Libero ullam, nesciunt soluta consectetur ea dolore .</p>
+         <div class="ms_d-inline p-2">
+              <button class="ms_btn_org ms_clr_wht">buy avada now</button>
+         </div>
+         <div class="ms_d-inline">
+             <button class="ms_btn_sky ms_clr_wht">contact us</button>
+         </div>
+     </div>
    </header>
 </template>
 
@@ -15,10 +25,33 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .ms_bg{
     background-image: url('../assets/img/1-hero-image.png');
     height: 100vh;
     background-size: cover;
+  }
+
+  .ms_btn_sky{
+    text-transform: uppercase;
+    background-color: #0369d7;
+    border: 0;
+    border-radius: 20px;
+    padding: 5px 20px;
+    font-weight: bold;
+  }
+
+  .ms_clr_wht{
+    color: white;
+  }
+
+  .ms_position_relative{
+    position: relative;
+  }
+
+   .ms_position_absolute{
+    position: absolute;
+    top: 35%;
+    left: 25%
   }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container p-4">
+       <div class="row">
+           <div class="col-4">
+              <img src="../assets/img/logo_seo_w_1x.png" alt="logo">
+           </div>
+           <div class="col-8">
+              <a v-for="(link, index) in links" :key="index" href="link.url">{{ link.name }}</a>
+              <div class="ms_d-inline ms_btn">
+                 <button class="ms_btn_org ms_clr_wht">Get in touch now</button>
+              </div>
+           </div>
+       </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'Navbar',
+
+   data: function(){
+     return{
+        links: [
+            {
+                name:'Home',
+                url:'#',
+            },
+
+            {
+                name:'Who We Are',
+                url:'',
+            },
+
+            {
+                name:'What We Do',
+                url:'',
+            },
+
+            {
+                name:'Where We Work',
+                url:'#',
+            },
+
+            {
+                name:'Careers',
+                url:'#',
+                btn:'apply'
+            },
+
+            {
+                name:'News',
+                url:'#',
+            },
+        ]
+     }
+   }
+}
+</script>
+
+<style lang="scss">
+ a{
+    text-decoration: none;
+    color: white;
+    padding-left: 20px;
+    &:hover{
+        color: #ffa837;
+    }
+ }
+
+ .ms_btn{
+   padding-left: 40px;
+ }
+
+ .ms_d-inline{
+    display: inline-block;
+ }
+
+ .ms_btn_org{
+    text-transform: uppercase;
+    background-color: #ffa837;
+    border: 0;
+    border-radius: 20px;
+    padding: 5px 20px;
+    font-weight: bold;
+ }
+</style>
