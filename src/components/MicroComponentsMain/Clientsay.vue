4d414a0fc75ac9e4175174b2ba22353d509910aa
Franpan94 <francescopanico94@libero.it>
2022-07-28 15:01:45 +0200

    finish bonus

--- src/components/MicroComponentsMain/Clientsay.vue
+++ src/components/MicroComponentsMain/Clientsay.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="text-center pt-5 pb-5">
     <h2 class="ms_font">Whatch Our Clients Say</h2>
-    <img src="../../assets/img/testimonials-2.jpg" alt="img" class="pt-3 ms_radius">
-    <p class="pt-3">Ability procedees from a fusion of skills, knowledge, understanding and imagination, <br> consolidated, by experience.</p>
-    <span class="ms_font">Luis Desalvo</span>
-    <span class="ms_upper">, creo tech</span>
+    <img :src="require(`../../assets/img/${testimonials[i].img}.jpg`)" alt="img" class="pt-3 ms_radius">
+    <p class="pt-3">{{ testimonials[i].paragraph }}</p>
+    <span class="ms_font">{{ testimonials[i].name }}</span>
+    <span class="ms_upper">{{ testimonials[i].work }}</span>
     <div class="container">
       <div class="row justify-content-center pt-5">
-        <div class="ms_circle"></div>
-        <div class="ms_circle_empty"></div>
+        <div v-for="(circle,counter) in 2" :key="counter" class="ms_circle" :class="isActivate(counter)"  @click="isActivateImg(counter)"></div>
       </div>
       <div class="row">
         <div class="col-3 mt-5 ms_bord" div v-for="(img,index) in imgs" :key="index">
@@ -25,7 +24,35 @@ export default {
 
   data: function(){
     return{
-      imgs:['avada-flame','the-avada','avada-rombo','avada-law']
+      i:0,
+      imgs:['avada-flame','the-avada','avada-rombo','avada-law'],
+      testimonials: [
+        {
+          img: 'testimonialman',
+          paragraph: 'Ability procedees from a fusion of skills, knowledge, understanding and imagination, consolidated, by experience.',
+          name:'Luis Desalvo',
+          work:', creo tech',
+        },
+
+        {
+          img: 'testimonialwoman',
+          paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, porro repellat velit accusamus quisquam voluptatibus!',
+          name:'Angelina Jolie',
+          work:', developer',
+        },
+      ]
+    }
+  },
+
+  methods:{
+    isActivate(position){
+      if(this.i === position){
+        return 'ms_active';
+      }
+    },
+
+    isActivateImg(number){
+      this.i = number;
     }
   }
 }
@@ -43,19 +70,17 @@ export default {
   text-transform: uppercase;
 }
 
-.ms_circle{
-  height: 25px;
-  background-color: $bg_black;
-  width: 25px;
-  border-radius: 50%;
+.ms_active{
+ background-color: $bg_black;
 }
 
-.ms_circle_empty{
+.ms_circle{
   height: 25px;
   border: 1px solid $bg_black;
   width: 25px;
   border-radius: 50%;
   margin-left: 10px;
+  cursor: pointer;
 }
 
 .ms_bord{
